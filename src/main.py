cb12a7a2ea6ec156870b900756497ecb7f2cd35d
Andrew Curthoys <andrew-curthoys@system76-pc.localdomain>
2020-02-21 19:58:32 -0600

    Initial commit. Wrote function Duke Cannon, Olivina Men, Fulton & Roark, and CBD for Life - output currently to .csv, need to implement functionality to write to Postgres

---
+++ src/main.py
@@ -0,0 +1,206 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
+from collections import namedtuple
+import csv
+import time
+
+
+def duke_cannon():
+    zip_code_list = zip_codes()
+    for zip_code in zip_code_list:
+        
+        # wait 10 seconds between calls to not overload them with requests
+        time.sleep(10)
+
+        # create connection to Firefox browser to Duke Cannon store locator
+        options = Options()
+        options.headless = True
+        driver = webdriver.Firefox()
+        driver.get("https://dukecannon.com/pages/store-locator")
+
+        # input zip code
+        zip_code_input = driver.find_element_by_id('storemapper-zip')
+        zip_code_input.send_keys(zip_code)
+
+        # submit zip code - use try/except method in case there is a pop-up window
+        submit_button = driver.find_element_by_id('storemapper-go')
+        try:
+            submit_button.click()
+        except:
+            popup = driver.find_element_by_id('popup')
+            popup_close = popup.find_element_by_class_name('close-modal')
+            popup_close.click()
+        submit_button.click()
+
+        # get the container with the list of store information
+        # then create a list of all stores
+        store_list_container = driver.find_element_by_id('storemapper-list')
+        store_list = store_list_container.find_elements_by_tag_name('li')
+
+        # create namedtuple to hold the store information
+        store_info = namedtuple('StoreInfo', 'store_id store_name address phone')
+
+        # loop through each store and store the store name, address, & phone number
+        # in a namedtuple called store_info
+        store_data = []
+        for store in store_list:
+            store_id = store.get_attribute('data-idx')
+            store_name = store.find_element_by_class_name('storemapper-title').text
+            try:
+                address = store.find_element_by_class_name('storemapper-address').text
+            except:
+                address = "No address found"
+            try:
+                phone = store.find_element_by_class_name('storemapper-phone').text
+            except:
+                phone = "No phone number found"
+            store = store_info(store_id, store_name, address, phone)
+            store_data.append(store)
+
+        # write output to csv
+        with open('../data/duke_cannon.csv', 'a') as f:
+            w = csv.writer(f)
+            w.writerow(('store_id', 'store_name', 'address', 'phone_number'))
+            w.writerows([store.store_id, store.store_name, store.address, store.phone] for store in store_data)
+        
+        driver.quit()
+
+
+def olivina_men():
+    # create connection to Firefox browser to Olivina Men store locator
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox()
+    driver.get("https://olivinamen.com/pages/store-locator")
+
+    # get the container with the list of store information
+    # then create a list of all stores
+    store_list_container = driver.find_element_by_id('storemapper-list')
+    store_list = store_list_container.find_elements_by_tag_name('li')
+
+    # create namedtuple to hold the store information
+    store_info = namedtuple('StoreInfo', 'store_id store_name address phone')
+
+    # loop through each store and store the store name, address, & phone number
+    # in a namedtuple called store_info
+    store_data = []
+    for store in store_list:
+        store_id = store.get_attribute('data-idx')
+        store_name = store.find_element_by_class_name('storemapper-title').text
+        try:
+            address = store.find_element_by_class_name('storemapper-address').text
+        except:
+            address = "No address found"
+        try:
+            phone = store.find_element_by_class_name('storemapper-phone').text
+        except:
+            phone = "No phone number found"
+        store = store_info(store_id, store_name, address, phone)
+        store_data.append(store)
+
+    # write output to csv
+    with open('../data/olivina_men.csv', 'w') as f:
+        w = csv.writer(f)
+        w.writerow(('store_id', 'store_name', 'address', 'phone_number'))
+        w.writerows([store.store_id, store.store_name, store.address, store.phone] for store in store_data)
+
+    driver.quit()
+
+
+def fulton_and_roark():
+    # create connection to Firefox browser to Fulton and Roark store locator
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox()
+    driver.get("https://fultonandroark.com/pages/stockists")
+
+    # get the container with the list of store information
+    # then create a list of all stores
+    store_list_container = driver.find_element_by_id('storemapper-list')
+    store_list = store_list_container.find_elements_by_tag_name('li')
+
+    # create namedtuple to hold the store information
+    store_info = namedtuple('StoreInfo', 'store_id store_name address phone')
+
+    # loop through each store and store the store name, address, & phone number
+    # in a namedtuple called store_info
+    store_data = []
+    for store in store_list:
+        store_id = store.get_attribute('data-idx')
+        store_name = store.find_element_by_class_name('storemapper-title').text
+        try:
+            address = store.find_element_by_class_name('storemapper-address').text
+        except:
+            address = "No address found"
+        try:
+            phone = store.find_element_by_class_name('storemapper-phone').text
+        except:
+            phone = "No phone number found"
+        store = store_info(store_id, store_name, address, phone)
+        store_data.append(store)
+
+    # write output to csv
+    with open('../data/fulton_and_roark.csv', 'w') as f:
+        w = csv.writer(f)
+        w.writerow(('store_id', 'store_name', 'address', 'phone_number'))
+        w.writerows([store.store_id, store.store_name, store.address, store.phone] for store in store_data)
+
+    driver.quit()
+
+
+def cbd_for_life():
+    # create connection to Firefox browser to CBD for Life store locator
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox()
+    driver.get("https://cbdforlife.us/store-locator/")
+
+    # get the container with the list of store information
+    # then create a list of all stores
+    store_list_container = driver.find_element_by_id('storemapper-list')
+    store_list = store_list_container.find_elements_by_tag_name('li')
+
+    # create namedtuple to hold the store information
+    store_info = namedtuple('StoreInfo', 'store_id store_name address phone')
+
+    # loop through each store and store the store name, address, & phone number
+    # in a namedtuple called store_info
+    store_data = []
+    for store in store_list:
+        store_id = store.get_attribute('data-idx')
+        store_name = store.find_element_by_class_name('storemapper-title').text
+        try:
+            address = store.find_element_by_class_name('storemapper-address').text
+        except:
+            address = "No address found"
+        try:
+            phone = store.find_element_by_class_name('storemapper-phone').text
+        except:
+            phone = "No phone number found"
+        store = store_info(store_id, store_name, address, phone)
+        store_data.append(store)
+
+    # write output to csv
+    with open('../data/cbd_for_life.csv', 'w') as f:
+        w = csv.writer(f)
+        w.writerow(('store_id', 'store_name', 'address', 'phone_number'))
+        w.writerows([store.store_id, store.store_name, store.address, store.phone] for store in store_data)
+
+    driver.quit()
+
+
+def zip_codes():
+    minneapolis = [
+        55401, 55402, 55403, 55404, 55405, 55406, 55407, 55408, 55409, 55410,
+        55411, 55412, 55413, 55414, 55415, 55416, 55417, 55418, 55419, 55421,
+        55423, 55430, 55450, 55454, 55455
+    ]
+    return minneapolis
+
+
+if __name__ == "__main__":
+    olivina_men()
+    fulton_and_roark()
+    cbd_for_life()
+    duke_cannon()
